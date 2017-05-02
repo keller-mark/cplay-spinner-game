f15205f2053e6e84e327abcd38e0038c3daf0638
jacob5555555555 <jacobeliasprinz@gmail.com>
2017-05-02 13:58:35 -0400

    Commented the header as well

--- game_logic.h
+++ game_logic.h
@@ -1,5 +1,7 @@
 #include "hardware.h"
 
+/*Functions that deal with the logic of the game itself*/
+
 void cpyPixel(uint8_t dest[3], uint8_t src[3]);
 void rotate(Pixels pixels, uint8_t color[3]);
 void setColor(uint8_t color[3]);
